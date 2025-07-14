83a65c3a5f4f1dd2da21615a7987933c69b0e1cd
mirulasyrani <mirulasyrani@gmail.com>
2025-07-15 05:07:27 +0800

    restored deleted files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,15 +3,15 @@ import { ToastContainer } from 'react-toastify';
 import { AuthProvider } from './context/authContext';
 
 import Header from './components/Header';
-import PrivateRoute from './components/PrivateRoute'; // Re-add this import
+import PrivateRoute from './components/PrivateRoute';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import DashboardPage from './pages/DashboardPage';
 
 function App() {
   return (
-    <AuthProvider>
-      <Router>
+    <Router>
+      <AuthProvider>
         <div className="bg-slate-100 min-h-screen text-slate-800">
           <Header />
           <main className="container mx-auto p-4">
@@ -41,8 +41,8 @@ function App() {
             theme="light"
           />
         </div>
-      </Router>
-    </AuthProvider>
+      </AuthProvider>
+    </Router>
   );
 }
 
